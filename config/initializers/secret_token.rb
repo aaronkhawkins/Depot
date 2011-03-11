7bdbb442069a114bb5ab9b2db117f30670abeabd
Aaron K. Hawkins <akh@aaronkhawkins.com>
2011-03-11 15:55:48 -0700

    A1 and A2 Depot scaffold and prettier listings

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '0f22141862015bf0147900ac9237b25ce2e5093400131a9fb149319c655cb1c85b3cfd3c414d61d360e1271cbe0301b5b06cbfccace19cafcbb14cb2c5a39f98'
